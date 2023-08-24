cfc73b349fab57e58944feef7714239779bc2124
Paolo Brasolin <paolo.brasolin@gmail.com>
2023-08-24 18:51:47 +0200

    Sketch PDF generation

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 css:
 	yarn run build:css
 
+pdf:
+	yarn run build:pdf
+
 www:
 	bundle exec jekyll build
 
---
+++ _puppeteer/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const puppeteer = require("puppeteer");
+
+// NOTE: remember to install missing dependencies.
+// https://gist.github.com/winuxue/cfef08e2f5fe9dfc16a1d67a4ad38a01
+// libatk-bridge2.0-0 libxkbcommon-x11-0 libgbm-dev
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ["--allow-file-access-from-files", "--enable-local-file-accesses"],
+  });
+  const page = await browser.newPage();
+  await page.goto(`file://${__dirname}/../_site/index.html`, {
+    waitUntil: "networkidle2",
+  });
+  await page.pdf({
+    path: "paolo-brasolin_cv.pdf",
+    format: "a4",
+  });
+
+  await browser.close();
+})();
