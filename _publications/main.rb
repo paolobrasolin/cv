bc7a273b2f40e3f9db10d9c055de869bc653498b
Paolo Brasolin <paolo.brasolin@gmail.com>
2023-10-05 10:13:31 +0200

    Improve bib style

--- _publications/main.rb
+++ _publications/main.rb
@@ -3,8 +3,10 @@ require 'citeproc'
 require 'csl/styles'
 require 'yaml'
 
-cp = CiteProc::Processor.new style: 'apa', format: 'html'
+cp = CiteProc::Processor.new style: 'apa-no-ampersand', format: 'html'
 cp.import BibTeX.open(ARGV[0]).to_citeproc
 items = cp.render(:bibliography, cp.data)
-items = items.map { |item| "- " + item }
+items = items.map do |item|
+    "- " + item.tr('{}', '').gsub(/<i>(.*?)<\/i>/, '_\1_').gsub(URI.regexp(['http','https','ftp']), '[\0](\0)')
+end
 puts ({ "title" => "Publications", "body" => items.join("\n") }.to_yaml)
