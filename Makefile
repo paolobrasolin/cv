7199b02b89a5251fc17a5feccf5b51e1aacb963e
Paolo Brasolin <paolo.brasolin@gmail.com>
2024-02-16 16:22:40 +0100

    Dockerize puppeteer

--- Makefile
+++ Makefile
@@ -1,8 +1,29 @@
+log := @printf "\n\u001b[7m ■ %s \u001b[0m\n"
+
 css:
 	yarn run build:css
 
 pdf:
 	yarn run build:pdf
+
+assets/paolo-brasolin-cv.pdf:
+	$(log) "Create target path $(dir $@)"
+	mkdir -p $(dir $@)
+	$(log) "Ensure a target file with correct ownership exists"
+	touch $@
+	$(log) "Set lax permissions on target to let docker overwrite it"
+	chmod -R o+w $@
+	$(log) "Regenerate target file"
+	docker run -it --init --cap-add=SYS_ADMIN --rm \
+		--volume "$(CURDIR)/_puppeteer/:/home/pptruser/_puppeteer/" \
+		--volume "$(CURDIR)/_site/:/home/pptruser/_site/" \
+		--volume "$(CURDIR)/assets/:/home/pptruser/assets/" \
+		ghcr.io/puppeteer/puppeteer:latest \
+		node _puppeteer/index.js \
+			--source "file:///home/pptruser/_site/index.html" \
+			--target "/home/pptruser/$@"
+	$(log) "Restore strict permissions on target"
+	chmod -R o-w $@
 
 www:
 	bundle exec jekyll build
--- _puppeteer/index.js
+++ _puppeteer/index.js
@@ -1,23 +1,26 @@
-"use strict";
+#!/usr/bin/env node
 
 const puppeteer = require("puppeteer");
-
 // NOTE: remember to install missing dependencies.
 // https://gist.github.com/winuxue/cfef08e2f5fe9dfc16a1d67a4ad38a01
 // libatk-bridge2.0-0 libxkbcommon-x11-0 libgbm-dev
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+
+const argv = yargs(hideBin(process.argv))
+  .option('source', { describe: 'address of HTML source' })
+  .option('target', { describe: 'path of PDF output' })
+  .demandOption(['source', 'target'])
+  .argv;
 
 (async () => {
   const browser = await puppeteer.launch({
     args: ["--allow-file-access-from-files", "--enable-local-file-accesses"],
   });
   const page = await browser.newPage();
-  await page.goto(`file://${__dirname}/../_site/index.html`, {
-    waitUntil: "networkidle2",
-  });
-  await page.pdf({
-    path: `${__dirname}/../assets/paolo-brasolin-cv.pdf`,
-    format: "a4",
-  });
-
+  await page.goto(argv.source, { waitUntil: "networkidle2" });
+  await page.pdf({ path: argv.target, format: "a4" });
   await browser.close();
 })();
+
